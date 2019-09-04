ef0910575f87afa2c4d2bc30784a5563d3b17ff3
FTruglio <frank@truglioventures.com>
2019-09-04 22:55:48 +0100

    removed queue buffer after refactor this is not necessary with using the offsetting delay

--- services/queue/process.js
+++ services/queue/process.js
@@ -49,59 +49,54 @@ module.exports = async ({
 		for (let index = 0; index < messages.length; index++) {
 			const message = messages[index];
 			const currentMessage = Object.assign({}, message, { batchId });
-			if (
-				isPastQueueBuffer({ messageCreatedAt: message.createdAt }) ||
-				removeBuffer
+			const { source, topic } = message;
+			// Test processing works.
+			if (source === "test-script") {
+				const [error, result] = await ProcessMessageTest({
+					message: currentMessage
+				});
+				if (error) {
+					await handleCleanUpOnError({
+						message: currentMessage,
+						batchId,
+						errorMessage: error ? error.message : ""
+					});
+				} else {
+					handleProcessedMessage({ message: currentMessage });
+				}
+			} else if (process.env.APP_URL && source === process.env.APP_URL) {
+				// If the source is the APP_URL that means this message should be published
+				// to all subscribers and not processed internally with the script registry.
+				const [error, result] = await PublishMessage({
+					message: currentMessage
+				});
+				if (error) {
+					await handleCleanUpOnError({
+						message: currentMessage,
+						batchId,
+						errorMessage: error ? error.message : ""
+					});
+				} else {
+					handleProcessedMessage({ message: currentMessage });
+				}
+			} else if (
+				source !== process.env.APP_URL &&
+				messageHandlers[`${topic}`]
 			) {
-				const { source, topic } = message;
-				// Test processing works.
-				if (source === "test-script") {
-					const [error, result] = await ProcessMessageTest({
-						message: currentMessage
+				// If the source is not the current APP and their is a script then
+				// Use the script with the key === to the message topic
+				const [error, result] = await messageHandlers[`${topic}`]({
+					message
+				});
+				if (error) {
+					// eslint-disable-next-line no-await-in-loop
+					await handleCleanUpOnError({
+						message: currentMessage,
+						batchId,
+						errorMessage: error ? error.message : ""
 					});
-					if (error) {
-						await handleCleanUpOnError({
-							message: currentMessage,
-							batchId,
-							errorMessage: error ? error.message : ""
-						});
-					} else {
-						handleProcessedMessage({ message: currentMessage });
-					}
-				} else if (process.env.APP_URL && source === process.env.APP_URL) {
-					// If the source is the APP_URL that means this message should be published
-					// to all subscribers and not processed internally with the script registry.
-					const [error, result] = await PublishMessage({
-						message: currentMessage
-					});
-					if (error) {
-						await handleCleanUpOnError({
-							message: currentMessage,
-							batchId,
-							errorMessage: error ? error.message : ""
-						});
-					} else {
-						handleProcessedMessage({ message: currentMessage });
-					}
-				} else if (
-					source !== process.env.APP_URL &&
-					messageHandlers[`${topic}`]
-				) {
-					// If the source is not the current APP and their is a script then
-					// Use the script with the key === to the message topic
-					const [error, result] = await messageHandlers[`${topic}`]({
-						message
-					});
-					if (error) {
-						// eslint-disable-next-line no-await-in-loop
-						await handleCleanUpOnError({
-							message: currentMessage,
-							batchId,
-							errorMessage: error ? error.message : ""
-						});
-					} else {
-						handleProcessedMessage({ message: currentMessage });
-					}
+				} else {
+					handleProcessedMessage({ message: currentMessage });
 				}
 			}
 		}
