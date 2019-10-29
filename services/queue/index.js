d8474250c6ff3328fe6d8297d14ed9996651e72b
FTruglio <frank@truglioventures.com>
2019-10-29 13:57:08 -0400

    updates

--- services/queue/index.js
+++ services/queue/index.js
@@ -50,7 +50,7 @@ module.exports = async ({ removeBuffer = false }) => {
 
 		const [updateManyError] = await MessageQueuedResource.updateMany({
 			query: { nodeId },
-			object: { nodeId: null, status: "locked", assignedAt: null }
+			object: { nodeId, status: "locked", assignedAt: null }
 		});
 
 		if (updateManyError) throw new Error(updateManyError);
